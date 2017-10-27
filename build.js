d927e710c16761a20274b7ebc9e73f7c9acf0771
Malcolm Diller <dillerm@oregonstate.edu>
2017-10-26 19:44:06 -0700

    added config system and filtering for jobs/projects

--- build.js
+++ build.js
@@ -1,10 +1,22 @@
 var fs = require("fs");
 var child_process = require("child_process");
 
-var build_dir = "build/";
-if (!fs.existsSync(build_dir)) {
-	fs.mkdirSync(build_dir);
+var config_file = "config.json"
+
+// Default config values
+var config = {
+	build_dir: "build"
 }
+// Load config json data
+if (fs.existsSync(config_file)) {
+	Object.assign(config, JSON.parse(fs.readFileSync(config_file, "utf8")));
+}
+
+if (!fs.existsSync(config.build_dir)) {
+	fs.mkdirSync(config.build_dir);
+}
+
+
 
 function fixCSS(text) {
 	var defs_pattern = /:root {([^}]+)}/;
@@ -52,16 +64,44 @@ function projectToHTML(project) {
 }
 
 // Just inserting these as strings because it isn't worth loading js-dom
-function addExperience(text, jobs) {
+function addExperience(text, jobs, config) {
 	jobs_text = "";
+	if (config.experience) {
+		var new_jobs = [];
+		config.experience.forEach(company => {
+			var job = jobs.find(job => job.company.toLowerCase() === company.toLowerCase());
+			if (job) {
+				new_jobs.push(job);
+			}
+			else {
+				console.log(`couldn't find company: ${company}`);
+			}
+		});
+		jobs = new_jobs;
+	}
+
 	jobs.forEach(job => {
 		jobs_text += jobToHTML(job)
 	});
 	return text.replace("<!-- experience -->", jobs_text);
 }
 
-function addProjects(text, projects) {
+function addProjects(text, projects, config) {
 	projects_text = "";
+	if (config.projects) {
+		var new_projects = [];
+		config.projects.forEach(title => {
+			var project = projects.find(project => project.title.toLowerCase() === title.toLowerCase());
+			if (project) {
+				new_projects.push(project);
+			}
+			else {
+				console.log(`couldn't find project: ${title}`);
+			}
+		});
+		projects = new_projects;
+	}
+
 	projects.forEach(project => {
 		projects_text += projectToHTML(project)
 	});
@@ -73,18 +113,18 @@ var resume_json = JSON.parse(fs.readFileSync("resume.json", "utf8"));
 
 // Copy over all relevant files
 var html = fs.readFileSync("index.html", "utf8");
-html = addExperience(html, resume_json.experience);
-html = addProjects(html, resume_json.projects);
-fs.writeFileSync(`${build_dir}index.html`, html);
+html = addExperience(html, resume_json.experience, config);
+html = addProjects(html, resume_json.projects, config);
+fs.writeFileSync(`${config.build_dir}/index.html`, html);
 
 
 var css = fs.readFileSync("style.css", "utf8");
 css = fixCSS(css);
-fs.writeFileSync(`${build_dir}style.css`, css);
+fs.writeFileSync(`${config.build_dir}/style.css`, css);
 
 
 // Build the html and css as a pdf
-child_process.exec(`wkhtmltopdf -B 0 -L 0 -R 0 -T 0 --page-width 8.5in --page-height 11in ${build_dir}index.html ${build_dir}resume.pdf`, (err, stdout, stderr) => {
+child_process.exec(`wkhtmltopdf -B 0 -L 0 -R 0 -T 0 --page-width 8.5in --page-height 11in ${config.build_dir}/index.html ${config.build_dir}/resume.pdf`, (err, stdout, stderr) => {
 	if (err) {
 		console.log("There was a problem running wkhtmltopdf. Make sure you have it installed");
 		return;
