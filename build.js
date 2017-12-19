1ad82d045be4152c5e24da8e4e879ba5d50aa40f
Malcolm Diller <dillerm@oregonstate.edu>
2017-12-19 15:31:12 -0800

    added first version of resume_simple

--- build.js
+++ build.js
@@ -41,10 +41,13 @@ function jsonToUl(data) {
 }
 
 Handlebars.registerHelper("textToHTML", textToHTML);
+Handlebars.registerHelper("jsonToUl", jsonToUl);
 Handlebars.registerHelper("linkText", function(link, text) {
 	return link ? `<a href="${link}">${text}</a>` : textToHTML(text);
 });
-Handlebars.registerHelper("jsonToUl", jsonToUl);
+Handlebars.registerHelper("simpleLink", function() {
+	return (this.link && this.link.startsWith("http")) ? this.link : this.text;
+})
 
 function copyRecursive(src, dest) {
 	if (fs.existsSync(src) && fs.statSync(src).isDirectory()) {
@@ -100,15 +103,23 @@ if (config.experience) {
 // Copy over all relevant files
 
 // resume.html
+var css = fs.readFileSync("resume.css", "utf8");
+resume_json.style = `<style>${fixCSS(css)}</style>`;
+
 var template = fs.readFileSync("resume.handlebars", "utf8");
 template = Handlebars.compile(template);
 var html = template(resume_json);
 fs.writeFileSync(`${build_directory}/resume.html`, html);
 
-// style.css
-var css = fs.readFileSync("style.css", "utf8");
-css = fixCSS(css);
-fs.writeFileSync(`${build_directory}/style.css`, css);
+// resume_simple.html
+css = fs.readFileSync("resume_simple.css", "utf8");
+resume_json.style = `<style>${fixCSS(css)}</style>`;
+
+template = fs.readFileSync("resume_simple.handlebars", "utf8");
+template = Handlebars.compile(template);
+html = template(resume_json);
+fs.writeFileSync(`${build_directory}/resume_simple.html`, html);
+
 
 // resume.json
 var text = fs.readFileSync("resume.json", "utf8");
@@ -134,9 +145,19 @@ if (config.build_pdf == undefined || config.build_pdf) {
 		if (stdout != "") {
 			console.log(stdout);
 		}
-		finish_build();
+		// finish_build();
+	});
+	child_process.exec(`wkhtmltopdf -B 0 -L 0 -R 0 -T 0 --disable-javascript --page-width 8.5in --page-height 11in ${build_directory}/resume_simple.html ${build_directory}/resume_simple.pdf`, (err, stdout, stderr) => {
+		if (err) {
+			console.log("There was a problem running wkhtmltopdf. Make sure you have it installed");
+			return;
+		}
+		if (stdout != "") {
+			console.log(stdout);
+		}
+		// finish_build();
 	});
 }
 else {
-	finish_build();
+	// finish_build();
 }
